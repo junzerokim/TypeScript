b40797d394e56bd0a2e0cb305106e3404f1a5483
Cos Kim <junzerokim@gmail.com>
2022-12-19 19:02:42 +0900

    TypeScript Type Practice

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var 이름 = 'Kim';
+function changeNumber(n) {
+  var result = [];
+  n.map(function (a) {
+    if (typeof a === 'string') {
+      result.push(Number(a));
+    } else {
+      result.push(a);
+    }
+  });
+  return result;
+}
+console.log(changeNumber(['1', 2, '3']));
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-let result: string | number = 123;
+function changeNumber(n: (number | string)[]) {
+  let result: number[] = [];
+  n.map((a) => {
+    if (typeof a === 'string') {
+      result.push(Number(a));
+    } else {
+      result.push(a);
+    }
+  });
+  return result;
+}
+
+console.log(changeNumber(['1', 2, '3']));
